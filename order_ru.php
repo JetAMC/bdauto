5bfc1019003fa07189655da893fd45da24c23fde
JetAMC <technics30@inbox.lv>
2020-03-20 08:48:26 +0200

    added thank you page

--- order_ru.php
+++ order_ru.php
@@ -21,13 +21,60 @@ $produkt = isset($_POST["Editbox7"]) ? $_POST["Editbox7"] : "";
 // Sending
 mail('info@bdauto.lv', 'Заявка с Landing Page полировка RU', $name . "\n" . $kod . "\n" . $email . "\n" . $source . "\n" . $medium . "\n" . $campaign . "\n" . $produkt);
 ?>
+<head>
+	<title>Lukturu pulēšana | SIA Baltic Detailing</title>
+    <meta charset=utf-8>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/media-query.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Roboto:700&display=swap" rel="stylesheet"> 
+</head>
 <body>
-<div class="main_block">
-	<div id="bg">
-		<div class="container">
-			<h1 style="font-size: 35px;" class="h1">Спасибо за заявку!<br />В ближайшее время, мы с вами свяжемся.</h1>
+<div class="header">
+    <div class="header__container container">
+        <div class="header__logo">
+            <img alt="" src="images/logo.png">
+        </div>
+        <div class="header__contact">
+            <div class="header__phone-number">
+                <img alt="" src="images/icons/contact.png">
+                    <a href="tel:+37125925977">+371 259-259-77</a>
+            </div>
+            <div class="header__address">
+                <img alt="" src="images/icons/location.png">
+                    <span>Rīga, Līzuma iela 1 k-12 (около T/C Alfa)</span>
+            </div>
+            <div class="header__languages">
+                <a href="order.php"><span>LV</span></a>
+                <a class="active" href="order_ru.php"><span>RU</span></a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="thank-you">
+	<div class="thank-you__container container">
+		<div class="thank-you__text-area">
+			<div class="thank-you__final-words">
+				<h1>Спасибо за заявку!</h1>
+			</div>
+			<div class="thank-you__small-text">
+				<p>В ближайшее время, мы с вами свяжемся.</p>
+			</div>
 		</div>
 	</div>
+</div>
+<div class="footer">
+    <div class="footer__container container">
+        <div class="footer__left-side">
+            <p>SIA Baltic Detailing &copy; 2014 - 2020</p>
+        </div>
+        <div class="footer__right-side">
+            <p>Rīga, Līzuma iela 1 k-12 | www.bdauto.lv</p>
+        </div>
+    </div>
 </div>
 </body>
 </html>
